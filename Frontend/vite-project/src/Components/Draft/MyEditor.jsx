9cdaa54417e5b26af0a68f6ba1bfafc1b33708bf
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-01-23 12:03:34 +0530

    tailwind base layer directive added for reverting tailwind  default style

--- Frontend/vite-project/src/Components/Draft/MyEditor.jsx
+++ Frontend/vite-project/src/Components/Draft/MyEditor.jsx
@@ -3,7 +3,9 @@ import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.snow.css";
 
 export default function MyEditor() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(
+    `<p>normal</p><h1>heading</h1><h2>heading 2</h2><p><strong>Bold</strong></p><p><em>Italic</em></p><p><u>Underline</u></p><p><s>Strikethrough</s></p><blockquote>Quote</blockquote><ol><li data-list="ordered"><span class="ql-ui" contenteditable="false"></span>order list1</li><li data-list="ordered"><span class="ql-ui" contenteditable="false"></span>order list 2</li><li data-list="bullet"><span class="ql-ui" contenteditable="false"></span>unorder list 1</li><li data-list="bullet"><span class="ql-ui" contenteditable="false"></span>unorder list2</li></ol><p class="ql-indent-3">left indent</p><p><br></p><p class="ql-indent-3"><br></p><p><br></p>`
+  );
 
   const modules = {
     toolbar: [
@@ -36,17 +38,18 @@ export default function MyEditor() {
 
   return (
     <>
-      <div className=" w-3/4 m-auto">
+      <div className=" no-tailwindcss-base  w-3/4 m-auto">
         <ReactQuill
+          className=""
           modules={modules}
           formats={formats}
           theme="snow"
           value={value}
           onChange={setValue}
         />
-        <div>
-          <h3>Preview:</h3>
-          <div className="border" dangerouslySetInnerHTML={{ __html: value }} />
+        <h3>Preview:</h3>
+        <div className="border p-2">
+          <div dangerouslySetInnerHTML={{ __html: value }} />
         </div>
       </div>
     </>
